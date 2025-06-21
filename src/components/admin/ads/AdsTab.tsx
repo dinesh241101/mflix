659ed4365e4c4b3ed095ceba41a414447379ea05
dinesh <dinesh2411kaushik@gmail.com>
2025-06-21 13:29:16 +0530

    admin removal from the home scree

--- src/components/admin/ads/AdsTab.tsx
+++ src/components/admin/ads/AdsTab.tsx
@@ -1,4 +1,3 @@
-
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle } from "@/components/ui/alert-dialog";
